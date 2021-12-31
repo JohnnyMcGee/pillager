424901037026963de46b351142f7566f211d2343
Johnny McGee <johnnymcgee.dev@gmail.com>
2021-12-31 16:39:09 -0500

    Add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import './screens/screens.dart';
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const MyApp());
 }
 
