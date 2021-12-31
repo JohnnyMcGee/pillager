64cfaa8f278f32ad2fdbcb18f55e6f6ec8f079f0
Johnny McGee <johnnymcgee.dev@gmail.com>
2021-12-31 11:07:27 -0500

    Add simple scaffold structure to home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(
-        "Welcome, Viking brother!"
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('Pillager'),
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 50.0,
+                vertical: 20.0,
+              ),
+              child: Text(
+                "Welcome Viking Brother!",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
