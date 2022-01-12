0a22ca537027da63b4939d4a36b0cd09f24de868
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-01-12 06:00:37 -0500

    Add .copyWith method to raid and required field for firestore docId

--- lib/models/raid.dart
+++ lib/models/raid.dart
@@ -8,8 +8,10 @@ class Raid extends Equatable {
   DateTime arrivalDate;
   List vikings;
   List loot;
+  String docId;
 
   Raid({
+    required this.docId,
     required this.location,
     required this.numOfShips,
     required this.arrivalDate,
@@ -18,18 +20,38 @@ class Raid extends Equatable {
   });
 
   @override
-  List<Object> get props => [location, numOfShips, arrivalDate, vikings, loot];
+  List<Object> get props =>
+      [docId, location, numOfShips, arrivalDate, vikings, loot];
 
   @override
   String toString() {
     return '''
-    {
+    Raid({
+      docId: $docId,
       location: $location,
       numOfShips: $numOfShips,
       arrivalDate: $arrivalDate,
       vikings: $vikings,
       loot: $loot
-    }
+    })
     ''';
+  }
+
+  Raid copyWith({
+    docId,
+    location,
+    numOfShips,
+    arrivalDate,
+    vikings,
+    loot,
+  }) {
+    return Raid(
+      docId: docId ?? this.docId,
+      location: location ?? this.location,
+      numOfShips: numOfShips ?? this.numOfShips,
+      arrivalDate: arrivalDate ?? this.arrivalDate,
+      vikings: vikings ?? this.vikings,
+      loot: loot ?? this.loot,
+    );
   }
 }
