85bccb5e4ce832a60cfafda40899fef82c7f273c
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-01-28 05:15:13 -0500

    Create color palette in a shared file

---
+++ lib/bloc/shared.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color black = Color.fromRGBO(0, 0, 0, 1);
+const Color darkElectricBlue = Color.fromRGBO(88,111,124,1);
+const Color redNCS = Color.fromRGBO(178,13,48,1);
+const Color ghostWhite = Color.fromRGBO(244,244,249,1); 
+const Color ashGray = Color.fromRGBO(201,206,189,1); 
+
