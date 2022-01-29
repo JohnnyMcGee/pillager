d9097eaf50d3f4666169a14adefe3b2b919d6e18
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-01-29 09:14:10 -0500

    Refactor/remove unnecessary layout widgets

--- lib/widgets/sign_in.dart
+++ lib/widgets/sign_in.dart
@@ -19,135 +19,115 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Stack(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 40, bottom: 30),
-                    child: Text(
-                      'Welcome back!',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+      child: SingleChildScrollView(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Flexible(
+              fit: FlexFit.loose,
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 550),
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
                     children: [
-                      const Flexible(
-                        flex: 2,
-                        child: SizedBox(),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 40, bottom: 30),
+                        child: Text(
+                          'Welcome back!',
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
                       ),
-                      Flexible(
-                        flex: 5,
-                        child: Container(
-                          constraints: const BoxConstraints(maxWidth: 450),
-                          child: Form(
-                            key: _formKey,
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 10),
-                                  child: TextFormField(
-                                    initialValue: '',
-                                    onChanged: (val) {
-                                      setState(() => email = val);
-                                    },
-                                    decoration: const InputDecoration(
-                                        hintText: "email",
-                                        border: OutlineInputBorder(
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(30.0)),
-                                        ),
-                                        contentPadding: EdgeInsets.symmetric(
-                                            horizontal: 16)),
-                                  ),
-                                ),
-                                Padding(
-                                  padding:
-                                      const EdgeInsets.symmetric(vertical: 10),
-                                  child: TextFormField(
-                                    initialValue: '',
-                                    onChanged: (val) {
-                                      setState(() => password = val);
-                                    },
-                                    decoration: const InputDecoration(
-                                      hintText: "password",
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.all(
-                                            Radius.circular(30.0)),
-                                      ),
-                                      contentPadding:
-                                          EdgeInsets.symmetric(horizontal: 16),
-                                    ),
-                                  ),
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 30, bottom: 20),
-                                  child: ElevatedButton(
-                                    child: const Padding(
-                                      padding: EdgeInsets.all(8.0),
-                                      child: Text(
-                                        'Sign in',
-                                        style: TextStyle(fontSize: 16.0),
-                                      ),
-                                    ),
-                                    onPressed: () async {
-                                      context.read<SignInBloc>().add(
-                                          SignInEmailButtonPressed(
-                                              email: email,
-                                              password: password));
-                                    },
-                                  ),
-                                ),
-                              ],
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: TextFormField(
+                          initialValue: '',
+                          onChanged: (val) {
+                            setState(() => email = val);
+                          },
+                          decoration: const InputDecoration(
+                              hintText: "email",
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(30.0)),
+                              ),
+                              contentPadding:
+                                  EdgeInsets.symmetric(horizontal: 16)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: TextFormField(
+                          initialValue: '',
+                          onChanged: (val) {
+                            setState(() => password = val);
+                          },
+                          decoration: const InputDecoration(
+                            hintText: "password",
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30.0)),
                             ),
+                            contentPadding:
+                                EdgeInsets.symmetric(horizontal: 16),
                           ),
                         ),
                       ),
-                      const Flexible(
-                        flex: 2,
-                        child: SizedBox(),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30, bottom: 20),
+                        child: ElevatedButton(
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Sign in',
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ),
+                          onPressed: () async {
+                            context.read<SignInBloc>().add(
+                                SignInEmailButtonPressed(
+                                    email: email, password: password));
+                          },
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            const TextSpan(
+                              text: "Don't have an account yet? ",
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                            WidgetSpan(
+                              child: InkWell(
+                                onTap: () => widget.toggleSignInForm(),
+                                child: Text(
+                                  "Register",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .secondaryVariant,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: "Don't have an account yet? ",
-                          style: TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                        WidgetSpan(
-                          child: InkWell(
-                            onTap: () => widget.toggleSignInForm(),
-                            child: Text(
-                              "Register",
-                              style: TextStyle(
-                                fontSize: 14,
-                                color: Theme.of(context).colorScheme.secondaryVariant,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
