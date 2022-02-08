00a3683956c295b61c36077230282f5c2217640f
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-02-08 12:09:10 -0500

    Extract conditional logic from home screen to profile editor widget.

--- lib/widgets/profile_editor.dart
+++ lib/widgets/profile_editor.dart
@@ -1,15 +1,16 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:pillager/models/models.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pillager/bloc/bloc.dart';
+import 'package:pillager/services/services.dart';
 
 class ProfileEditor extends StatefulWidget {
-  final User user;
-  final Viking viking;
+  // final User user;
+  // final Viking viking;
 
-  const ProfileEditor({Key? key, required this.user, required this.viking})
-      : super(key: key);
+  const ProfileEditor({Key? key}) : super(key: key);
 
   @override
   State<ProfileEditor> createState() => _ProfileEditorState();
@@ -17,30 +18,23 @@ class ProfileEditor extends StatefulWidget {
 
 class _ProfileEditorState extends State<ProfileEditor> {
   final _formKey = GlobalKey<FormState>();
+  final user = AuthService().currentUser;
+
   String? _email;
   String? _firstName;
   String? _lastName;
   bool? _isBerserker;
   bool? _isEarl;
 
-  Map<String, Object> _profileUpdate() {
-    var changes = {
-      "email": _email,
-      "firstName": _firstName,
-      "lastName": _lastName,
-      "isBerserker": _isBerserker,
-      "isEarl": _isEarl,
-    }..removeWhere((k, v) => v == null);
-
-    return Map<String, Object>.from(changes);
-  }
-
   Future<void> _showDeleteDialog(BuildContext context) async {
     final bool? confirm = await showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text("Delete My Account", textAlign: TextAlign.center,),
+          title: const Text(
+            "Delete My Account",
+            textAlign: TextAlign.center,
+          ),
           content: const Text(
             """Are you sure you want to delete this account?
             You will not be able to recover it.""",
@@ -69,65 +63,113 @@ class _ProfileEditorState extends State<ProfileEditor> {
     }
   }
 
+  Map<String, Object> get _vikingUpdate {
+    final vikingUpdate = {
+      "firstName": _firstName,
+      "lastName": _lastName,
+      "isBerserker": _isBerserker,
+      "isEarl": _isEarl,
+    }..removeWhere((k, v) => v == null);
+
+    return Map<String, Object>.from(vikingUpdate);
+  }
+
+  void _updateProfile(BuildContext context, Viking viking) {
+    if (_email is String) {
+      user?.updateEmail(_email!);
+    }
+
+    final bloc = context.read<VikingBloc>();
+
+    bloc.add(UpdateViking(
+          viking: viking,
+          update: _vikingUpdate,
+        ));
+
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          const Text(
-            "Edit Profile",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20.0,
+    return BlocBuilder<VikingBloc, VikingState>(
+      builder: (context, state) {
+        final viking = state.vikings[user?.uid];
+
+        if (user is User && viking is Viking) {
+          return Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  "Edit Profile",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20.0,
+                  ),
+                ),
+                TextFormField(
+                  initialValue: _email ?? user!.email,
+                  onChanged: (value) => setState(() {
+                    _email = value;
+                  }),
+                ),
+                TextFormField(
+                  initialValue: _firstName ?? viking.firstName,
+                  onChanged: (value) => setState(() {
+                    _firstName = value;
+                  }),
+                ),
+                TextFormField(
+                  initialValue: _lastName ?? viking.lastName,
+                  onChanged: (value) => setState(() {
+                    _lastName = value;
+                  }),
+                ),
+                Switch(
+                    value: _isBerserker ?? viking.isBerserker,
+                    onChanged: (value) => setState(() {
+                          _isBerserker = value;
+                        })),
+                Switch(
+                    value: _isEarl ?? viking.isEarl,
+                    onChanged: (value) => setState(() {
+                          _isEarl = value;
+                        })),
+                const SizedBox(height: 20),
+                TextButton(
+                  child: const Text(
+                    "Delete My Account",
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () => _showDeleteDialog(context),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  child: const Text(
+                    "Update Profile",
+                    textAlign: TextAlign.center,
+                  ),
+                  onPressed: () => _updateProfile(context, viking),
+                ),
+              ],
             ),
-          ),
-          TextFormField(
-            initialValue: _email ?? widget.user.email,
-            onChanged: (value) => setState(() {
-              _email = value;
-            }),
-          ),
-          TextFormField(
-            initialValue: _firstName ?? widget.viking.firstName,
-            onChanged: (value) => setState(() {
-              _firstName = value;
-            }),
-          ),
-          TextFormField(
-            initialValue: _lastName ?? widget.viking.lastName,
-            onChanged: (value) => setState(() {
-              _lastName = value;
-            }),
-          ),
-          Switch(
-              value: _isBerserker ?? widget.viking.isBerserker,
-              onChanged: (value) => setState(() {
-                    _isBerserker = value;
-                  })),
-          Switch(
-              value: _isEarl ?? widget.viking.isEarl,
-              onChanged: (value) => setState(() {
-                    _isEarl = value;
-                  })),
-          const SizedBox(height: 20),
-          TextButton(
-            child: const Text(
-              "Delete My Account",
-              textAlign: TextAlign.center,
+          );
+        } else {
+          return SizedBox(
+            height: 225,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text("Loading Profile...",
+                    style: Theme.of(context).textTheme.headline5),
+                SpinKitFoldingCube(
+                    color: Theme.of(context).colorScheme.secondary),
+              ],
             ),
-            onPressed: () => _showDeleteDialog(context),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            child: const Text(
-              "Update Profile",
-              textAlign: TextAlign.center,
-            ),
-            onPressed: () => Navigator.pop(context, _profileUpdate()),
-          ),
-        ],
-      ),
+          );
+        }
+      },
     );
   }
 }
