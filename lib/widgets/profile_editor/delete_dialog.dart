efc3146bde867675da51fe98181dd677ed74afcd
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-02-11 12:25:29 -0500

    Require user to enter password in delete user dialog

--- lib/widgets/profile_editor/delete_dialog.dart
+++ lib/widgets/profile_editor/delete_dialog.dart
@@ -1,7 +1,20 @@
 part of './profile_editor.dart';
 
-class DeleteDialog extends StatelessWidget {
+class DeleteDialog extends StatefulWidget {
   const DeleteDialog({Key? key}) : super(key: key);
+
+  @override
+  State<DeleteDialog> createState() => _DeleteDialogState();
+}
+
+class _DeleteDialogState extends State<DeleteDialog> {
+  String? _password;
+
+  void _deleteUser() {
+    if (_password is String) {
+      context.read<SignInBloc>().add(DeleteAccount(_password as String));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +23,43 @@ class DeleteDialog extends StatelessWidget {
         "Delete My Account",
         textAlign: TextAlign.center,
       ),
-      content: const Text(
-        """Are you sure you want to delete this account?
-            You will not be able to recover it.""",
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(height: 30.0),
+          Text(
+            """Are you sure you want to delete this account?
+                You will not be able to recover it.""",
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.secondaryVariant),
+          ),
+          const SizedBox(height: 20.0),
+          PasswordFormField(
+            onChanged: (value) => setState(() {
+              _password = value;
+            }),
+            hintText: "password",
+          ),
+          const SizedBox(height: 50.0),
+        ],
       ),
       actions: [
-        ElevatedButton(
+        TextButton(
           onPressed: () {
-            context.read<SignInBloc>().add(DeleteAccount());
+            _deleteUser();
             Navigator.pop(context, true);
           },
-          child: const Text("Delete Account"),
+          child: const Text("Yes, Delete Account"),
+          style: ButtonStyle(
+              padding: MaterialStateProperty.all(const EdgeInsets.all(20.0))),
         ),
         ElevatedButton(
           onPressed: () {
             Navigator.pop(context, false);
           },
-          child: const Text("Cancel"),
+          child: const Text("Don't Delete My Account!"),
+          style: ButtonStyle(
+              padding: MaterialStateProperty.all(const EdgeInsets.all(20.0))),
         ),
       ],
     );
