eefea3674478d9237a1980c56f553b033da7ce50
Johnny McGee <johnnymcgee.dev@gmail.com>
2022-02-13 14:20:11 -0500

    Update meta tags in index.html to make the chrome header color match app

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:pillager/screens/screens.dart';
 import 'package:pillager/bloc/bloc.dart';
+import 'package:pillager/screens/screens.dart';
 import 'themes.dart';
 
 class PillagerApp extends StatelessWidget {
@@ -11,16 +12,13 @@ class PillagerApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
         title: 'Pillager - Management Solutions for the Modern Viking',
         home: Theme(
           data: ThemeData.from(colorScheme: scheme),
           child: BlocBuilder<SignInBloc, SignInState>(
               builder: (BuildContext context, state) {
-            return (state is LoggedIn)
-                ? const Home()
-                : const Authentication();
+            return (state is LoggedIn) ? const Home() : const Authentication();
           }),
         ));
   }
